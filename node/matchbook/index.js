ab804d8b103dc51e59332a35ac41743940a0e3e7
Lenny4 <alexandre.beaujour@hotmail.fr>
2019-06-20 19:48:24 +0200

    add country verification

--- node/matchbook/index.js
+++ node/matchbook/index.js
@@ -3,6 +3,9 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('../')(server);
 const port = process.env.PORT || 3000;
+const scrapeIt = require("scrape-it");
+// app.use(express.urlencoded());
+// app.use(express.json());
 
 const Env = require('./Env.js').Env;
 const Const = require('./Const.js').Const;
@@ -10,11 +13,21 @@ const SymfonyApi = require('./symfony-api/SymfonyApi.js').SymfonyApi;
 
 server.listen(port, () => {
     console.log('Server listening at port %d', port);
+    console.log('Verifying IP location ...');
+    scrapeIt("https://iplocation.com/", {
+        country: "table.result-table span.country_name"
+    }).then(({data, response}) => {
+        if (typeof response !== "undefined" && typeof response.statusCode !== "undefined" && typeof data !== "undefined" && typeof data.country !== "undefined") {
+            if (data.country === "Finland") {
+                console.log('Your are in Finland !');
+            } else {
+                throw new Error('Your are not located in Finland');
+            }
+        } else {
+            throw new Error('Error with iplocation.com');
+        }
+    })
 });
-
-// app.use(express.urlencoded());
-// app.use(express.json());
-
 
 io.on('connection', (socket) => {
     socket.on('event', function (data, fn) {
--- symfony/src/Controller/MainController.php
+++ symfony/src/Controller/MainController.php
@@ -3,17 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="main")
+     * @param SessionInterface $session
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(SessionInterface $session)
     {
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'username' => $session->get('username'),
+            'password' => $session->get('password'),
         ]);
     }
 }
